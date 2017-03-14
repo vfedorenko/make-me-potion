6bafe81159fe29ea72117e52a7d1d6a40989ce2a
Vlad Fedorenko <vfedo92@gmail.com>
2017-03-14 14:41:15 +0300

    Extracted strings

--- app/src/main/java/by/vfedorenko/makemepotion/presentation/ingredients/viewmodels/IngredientsViewModel.kt
+++ app/src/main/java/by/vfedorenko/makemepotion/presentation/ingredients/viewmodels/IngredientsViewModel.kt
@@ -1,9 +1,9 @@
 package by.vfedorenko.makemepotion.presentation.ingredients.viewmodels
 
 import android.app.AlertDialog
-import android.util.Log
 import android.view.View
 import android.widget.Toast
+import by.vfedorenko.makemepotion.R
 import by.vfedorenko.makemepotion.businesslogic.Alchemist
 import by.vfedorenko.makemepotion.businesslogic.data.IngredientsRepository
 import by.vfedorenko.makemepotion.presentation.ingredients.activities.IngredientsActivity
@@ -32,36 +32,34 @@ class IngredientsViewModel
 
     fun onMakeClick(v: View) {
         val potion = Alchemist().beginExperiment(adapter.getCheckedItems())
-        Log.d("111", "--------------------------------------- potion points = ${potion.points}")
 
         if (potion.points > 0) {
-            var msg = "Combine: "
+            var msg = v.context.getString(R.string.combine)
 
             potion.ingredients.forEach { msg += "${it.name}\n" }
 
-            msg += "\nTo learn:"
+            msg += v.context.getString(R.string.to_learn)
 
             potion.effects.forEach { msg += "\n${it.name} " }
 
             val builder = AlertDialog.Builder(v.context)
 
-            builder.setTitle("Best choice for you:")
+            builder.setTitle(R.string.potion_dialog_title)
                     .setMessage(msg)
-                    .setPositiveButton("Done", { _, _ ->
+                    .setPositiveButton(R.string.done, { _, _ ->
                         potion.ingredients.forEach { ingr ->
                             potion.effects.forEach { effect ->
-                                Log.d("111", "ingr: ${ingr.name} eff: ${effect.name}")
                                 repository.setIngredientEffectKnown(ingr, effect, true)
                             }
                         }
 
                         refreshData()
                     })
-                    .setNegativeButton("Cancel", null)
+                    .setNegativeButton(R.string.cancel, null)
 
             builder.create().show()
         } else {
-            Toast.makeText(v.context, "No potions available", Toast.LENGTH_SHORT).show()
+            Toast.makeText(v.context, R.string.no_potion_available, Toast.LENGTH_SHORT).show()
         }
     }
 }
